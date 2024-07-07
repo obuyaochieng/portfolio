acbbad6230388227d5b4829a6029078b20345ba5
Obuya <obuyaochieng9@gmail.com>
2024-07-07 04:10:32 +0300

    initial commit

---
+++ mportfolio/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # include our custom app urls here
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
